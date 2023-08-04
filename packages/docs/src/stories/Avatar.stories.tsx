13f5b3a90711024984bdd248735355bf2172a425
RaquelAlbuquerque1 <albuquerqueraquel396@gmail.com>
2023-08-04 10:06:16 -0300

    feat: created components in the storybook

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,26 @@
+import { Avatar, AvatarProps } from '@tcc/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/RaquelAlbuquerque1.png',
+    alt: 'Raquel Albuquerque',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
