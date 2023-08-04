7148b98892a031f6096185d807f8443b9441e99e
RaquelAlbuquerque1 <albuquerqueraquel396@gmail.com>
2023-08-04 08:33:21 -0300

    feat: created design system in react

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,19 @@
+import { Button, ButtonProps } from '@tcc/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+
+  args: {
+    children: 'Enviar',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'small',
+  },
+}
